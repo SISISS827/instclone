861e09136e4c5e437f715edd5c4aee6d93e8f0b6
Seongyeon Lee <winkite1226@gmail.com>
2022-10-06 03:51:05 +0900

    feed delete complete

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import Join, Login, LogOut, UploadProfile
+from .views import Join, Login, LogOut, UploadProfile, Follow, ViewFollow
 
 urlpatterns = [
     path('join', Join.as_view()),
     path('login', Login.as_view()),
     path('logout', LogOut.as_view()),
     path('profile/upload', UploadProfile.as_view()),
+    path('<str:username>/follow', Follow.as_view()),
+    path('view_follow/', ViewFollow.as_view())
 ]
